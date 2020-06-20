5266006990893996510942d7bddeedf986fcef1b
Luan Ribeiro <luanribeiros@gmail.com>
2020-06-20 18:58:06 -0300

    Creating card articles and making requests to api
    Creating articles card and requesting an api to feed the card data

--- src/components/ArticleBox/index.jsx
+++ src/components/ArticleBox/index.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import * as S from './styles';
 
+const url =
+  'https://core.ac.uk:443/api-v2/articles/search/search?page=1&pageSize=10&metadata=true&fulltext=true&citations=true&similar=true&duplicate=false&urls=true&faithfulMetadata=true&apiKey=zR8viEFqhpncQmJdYAMCuyXgO7IPboVr';
+
 const ArticleBox = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setArticles(res.data.data);
+      console.log(res.data.data);
+    });
+  }, []);
+
   return (
-    <S.ContainerArticleBox>
-      <S.Authors> Authors </S.Authors>
-      <S.Title> Title </S.Title>
-      <S.Type> Type </S.Type>
-      <S.Description> description ... </S.Description>
-      <S.Urls> Urls ... </S.Urls>
-      <S.Button>Add Favorites</S.Button>
-    </S.ContainerArticleBox>
+    <>
+      {articles.map((item) => (
+        <S.ContainerArticleBox key={item.id}>
+          <S.Authors>Author(s): {item.authors.map((author) => author)}</S.Authors>
+          <S.Title> Title: {item.title} </S.Title>
+          <S.Type> Types: {item.types.map((types) => types)} </S.Type>
+          <S.Description>
+            <strong>Description:</strong> {item.description ? item.description : 'Sem Description'}{' '}
+          </S.Description>
+          Url links:
+          <S.Urls>
+            {item.fulltextUrls.map((item) => (
+              <S.A as="a" href={item}>
+                links
+              </S.A>
+            ))}
+          </S.Urls>
+          <S.ContainerButton>
+            <S.Button>Add Favorites</S.Button>
+            <S.Button>Remove Favorites</S.Button>
+          </S.ContainerButton>
+        </S.ContainerArticleBox>
+      ))}
+    </>
   );
 };
 
--- src/components/ArticleBox/styles.js
+++ src/components/ArticleBox/styles.js
@@ -1,14 +1,37 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const ContainerArticleBox = styled.section`
   border-radius: 4px;
   border: 1px solid #cfcfcf;
   padding: 1rem;
   box-shadow: 1px 2px 5px #d1d1d1;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
 `;
-export const Authors = styled.h1``;
-export const Title = styled.h2``;
-export const Type = styled.h3``;
-export const Urls = styled.h4``;
-export const Description = styled.p``;
-export const Button = styled.button``;
+export const Authors = styled.h1`
+  padding-bottom: 0.5rem;
+`;
+export const Title = styled.h2`
+  padding-bottom: 0.5rem;
+`;
+export const Type = styled.h3`
+  padding-bottom: 0.5rem;
+`;
+export const Urls = styled(Link)`
+  padding: 0.5rem 0.5rem 0.5rem 0;
+`;
+export const A = styled.a`
+  padding: 0.5rem;
+`;
+export const Description = styled.p`
+  padding-bottom: 0.5rem;
+`;
+export const ContainerButton = styled.section`
+  display: flex;
+  margin-top: 10px;
+`;
+export const Button = styled.button`
+  margin-right: 10px;
+`;
